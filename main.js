bf4981d4c76f840834f2bc3bfe2dcbb10689e199
Spiderman <128212597+RonaldoandMessi@users.noreply.github.com>
2024-03-01 11:43:32 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("1.");
+for (i = 1; i <= 50; i++) {
+    console.log(i);
+}
+
+console.log("2.");
+for (i = 0; i < 51; i++) {
+    console.log(i);
+}
+
+console.log("3.");
+for (i = 1; i < 50; i++) {
+    console.log(i);
+}
+
+console.log("4.");
+for (i = 67; i <= 99; i++) {
+    console.log(i);
+}
+
+console.log("5.");
+for (i = 0; i <= 600; i++) {
+    console.log(i);
+}
